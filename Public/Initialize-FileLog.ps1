e08027ce1537af87ae641998979b2a71024df946
Craig Buchanan <craig.buchanan@cogniza.com>
2020-04-06 13:43:47 -0500

    tidy code; write-debug changes

--- Public/Initialize-FileLog.ps1
+++ Public/Initialize-FileLog.ps1
@@ -7,18 +7,16 @@ function Initialize-FileLog {
         [string]$Path,
 
         [Parameter(Position=1)]
-        # [ValidateSet("Information", "Warning", "Error")]
-        # [ValidateSet('None','Error','Warning','Information','Debug')]
-        # [string]$LogLevel = 'Error',
         [Levels]$LogLevel = [Levels]::Error,
 
         [Parameter(Position=2)]
         [string]$Delimiter=','
     )
 
-    Write-Debug "Path: $Path"
-    Write-Debug "LogLevel: $LogLevel"
-    Write-Debug "Delimiter: $Delimiter"
+    Write-Debug $MyInvocation.MyCommand.Name
+    # Write-Debug "Path: $Path"
+    # Write-Debug "LogLevel: $LogLevel"
+    # Write-Debug "Delimiter: $Delimiter"
 
     # $ResolvedPath = Resolve-Path $Path
 
