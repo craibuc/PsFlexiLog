cdcb1a3777692c33c733454da1615f83c5d6c122
Craig Buchanan <craig.buchanan@cogniza.com>
2020-04-08 09:45:00 -0500

    initial implementation

--- Private/Reset-EventLogLog.Tests.ps1
+++ Private/Reset-EventLogLog.Tests.ps1
@@ -1,9 +1,37 @@
+Import-Module PsFlexiLog -Force
+
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
-Describe "Reset-EventLogLog" {
-    It "does something useful" {
-        $true | Should -Be $false
+InModuleScope 'PsFlexiLog' {
+
+    Describe "Reset-EventLogLog" {
+
+        BeforeEach {
+            # arrange
+            Initialize-EventLogLog -LogLevel Debug -LogName 'Application' -Source 'Do-Something'
+
+            # act
+            Reset-EventLogLog
+        }
+
+        It "disables the EventLog log" {
+            $Script:Settings.EventLog.Enabled | Should -Be $false
+        }
+        It "sets the LogLevel to 'Error'" {
+            $Script:Settings.EventLog.LogLevel | Should -Be Error
+        }
+        It "sets the LogName to [null]" {
+            $Script:Settings.EventLog.LogName | Should -Be $null
+        }
+        It "sets the Source to [null]" {
+            $Script:Settings.EventLog.Source | Should -Be $null
+        }
+        It "sets the EventId to 1000" {
+            $Script:Settings.EventLog.EventId | Should -Be 1000
+        }
+
     }
+
 }
--- Private/Reset-EventLogLog.ps1
+++ Private/Reset-EventLogLog.ps1
@@ -1,3 +1,9 @@
 function Reset-EventLogLog {
 
+    $Script:Settings.EventLog.Enabled = $false
+    $Script:Settings.EventLog.LogLevel = [Levels]::Error
+    $Script:Settings.EventLog.LogName = $null
+    $Script:Settings.EventLog.Source = $null
+    $Script:Settings.EventLog.EventId = 1000
+
 }
