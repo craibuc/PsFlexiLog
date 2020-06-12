19bb36e20c9d3c9a27e13df74eafc3cd3d54092f
Craig Buchanan <craig.buchanan@cogniza.com>
2020-06-12 13:55:42 -0500

    Adding Symlink task

--- psakefile.ps1
+++ psakefile.ps1
@@ -2,6 +2,14 @@ FormatTaskName "-------- {0} --------"
 
 Task default -depends Publish
 
+Task Symlink {
+    $Module='PsFlexiLog'
+    $Here = Get-Location
+    Push-Location ~/.local/share/powershell/Modules
+    ln -s "$Here/$Module" $Module
+    Pop-Location
+}
+
 Task Publish {
     publish-module -name ./PsFlexiLog -Repository Lorenz
 }
